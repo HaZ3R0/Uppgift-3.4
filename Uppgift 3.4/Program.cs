2f5ebe602720868c6eb0fc721e9234f4267ab526
majo06005 <majo06005@E-PW0234K2>
2023-10-06 13:01:43 +0200

    Add project files.

---
+++ Uppgift 3.4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace Övning_3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur lång är låten?");
+            string svar = Console.ReadLine();
+            int mellanslagsindex = svar.IndexOf(" ");
+            string minutersvar = svar[..mellanslagsindex];
+            string sekundersvar = svar[(mellanslagsindex + 1)..];
+            int minuter = int.Parse(minutersvar);
+            int sekunder = int.Parse(sekundersvar);
+
+
+            switch (minuter)
+            {
+                case 0:
+                    Console.WriteLine("Din låt får inte visas");
+                    break;
+                
+                case 1:
+                    Console.WriteLine("Din låt får inte visas");
+                    break;
+
+                case 2:
+
+                    if (sekunder >= 45 || sekunder <= 60)
+                        {
+                        Console.WriteLine("Din låt får visas");
+                        }
+
+                    else
+                        {
+                        Console.WriteLine("Din låt får ej visas");
+                        }
+                    break;
+
+                case 3:
+                    Console.WriteLine("Din låt får visas");
+                    break;
+
+                case 4:
+                    if (sekunder <= 20 || sekunder <= 60)
+                        {
+                            Console.WriteLine("Din låt får visas");
+                        }
+                    else
+                        {
+                        Console.WriteLine("Din låt får ej visas");
+                        }
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
